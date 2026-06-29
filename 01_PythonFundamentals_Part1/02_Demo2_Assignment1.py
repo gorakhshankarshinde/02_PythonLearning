35d1352503e8f6658f5a81f05a2f024ad7d289e6
Gorakh Shinde <gorakhshankarshinde@gmail.com>
2026-06-29 16:24:08 +0200

    two new assignment added

---
+++ 01_PythonFundamentals_Part1/02_Demo2_Assignment1.py
@@ -0,0 +1,10 @@
+'''
+Write a program that asks the user for their name and age, then prints a sentence like
+"Hello, [Name]! You are [Age] years old."
+'''
+
+print("Hello! What is your name?")
+name = input() 
+print("How old are you?")
+age = input()
+print(f"Hello, {name}! You are {age} years old.")
---
+++ 01_PythonFundamentals_Part1/03_Demo3_Assignment2.py
@@ -0,0 +1,12 @@
+'''
+Q2. Take two numbers as input from the user and print their sum, difference, product, and quotient.
+'''
+print("Enter the first number:")
+num1 = float(input())
+print("Enter the second number:")
+num2 = float(input())
+
+print(f"Sum: {num1 + num2}") # Or you can use print("Sum:", num1 + num2)
+print(f"Difference: {num1 - num2}") # Or you can use print("Difference:", num1 - num2)
+print(f"Product: {num1 * num2}")
+print(f"Quotient: {num1 / num2 if num2 != 0 else 'Undefined (division by zero)'}")
