04ef2745cc1ad5083b415b3d3b1297556d475471
Gorakh Shinde <gorakhshankarshinde@gmail.com>
2026-07-27 11:11:08 +0200

    New changes added

---
+++ 04_PythonFundamentals_Part4/01_ObjectOrientedProgrammingInPython.py
@@ -0,0 +1,6 @@
+'''
+We are going to learn classes and objects in Python programming.
+
+# Class is a blue print of an object.
+# Object is an instance of a class.
+'''
---
+++ 04_PythonFundamentals_Part4/02_ClassesAndObjectsInPython.py
@@ -0,0 +1,12 @@
+'''
+We are going to learn classes and objects in Python programming.
+
+# Class is a blue print of an object.
+# Object is an instance of a class.
+'''
+
+#Class Student:
+
+
+
+
