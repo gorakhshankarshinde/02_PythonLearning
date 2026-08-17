3ff3efa86d18cdbefd80a5e51de2c61a1fffedf6
Gorakh Shinde <gorakhshankarshinde@gmail.com>
2026-08-17 21:59:46 +0200

    Added new changes

--- 04_PythonFundamentals_Part4/09_Encapsulation.py
+++ 04_PythonFundamentals_Part4/09_Encapsulation.py
@@ -17,20 +17,40 @@ In Python, encapsulation is achieved using access specifiers (public, protected,
 '''
 class Bank:
 
-    def __init__(self, name,accountNumber, balance):
+    def __init__(self, name,accountNumber, balance, FD_Balance):
         self.name = name  # private attribute
         self._balance = balance  # protected attribute because it is prefixed with a single underscore
         self.__accountNumber = accountNumber  # private attribute because it is prefixed with double underscores
+        self.__FD_Balance = FD_Balance # this is private attribute we have used.
 
-    def get_AccountNumber(self):
+    def get_AccountNumber(self): # This is getter function, nothing but just we named it as prefix with "get"
         return self.__accountNumber
 
+    def set_Balance(self, newBalance): # here we have created setter function, which is setting the value to the private variable.
+        self.__FD_Balance = newBalance
+        return self.__FD_Balance
 
-account1 = Bank("John Doe", 1155, 5000)
+        
+
+
+account1 = Bank("John Doe", 1155, 5000, 100000)
 
 print("Account Holder:", account1.name)
 print("Account Number:", account1.get_AccountNumber())
 print("Account Balance:", account1._balance)
+
+
+# Important we can access private variable outside like shown below.
+print(f"Current FD amount is Rs.{account1._Bank__FD_Balance}")
+
+
+
+print(f"New FD balance amount is Rs. {account1.set_Balance(200000)}")
+
+
+
+
+
 
 
 
